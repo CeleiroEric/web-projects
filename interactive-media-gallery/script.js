c5f6d4137f78058b084d8e5c5d95329daf2ae65e
Eric Celeiro <celeiroericwork@gmail.com>
2026-02-17 01:22:04 +0100

    Add image modal with open and close behavior

--- interactive-media-gallery/script.js
+++ interactive-media-gallery/script.js
@@ -16,3 +16,32 @@ filterButtons.forEach(button =>{
         })
     })
 })
+const modal = document.getElementById('modal');
+const fullImg = document.getElementById('full-img');
+const images = document.querySelectorAll('.gallery-container img');
+
+images.forEach(img => {
+    img.addEventListener('click', () => {
+        modal.style.display = 'flex'; 
+        fullImg.src = img.src; 
+    });
+});
+
+modal.addEventListener('click', (event) => {
+   
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+});
+
+const closeBtn = document.querySelector('.close');
+
+closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+        modal.style.display = 'none';
+    }
+});
